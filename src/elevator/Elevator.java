ef329d130ce3f2b888e35ebb70121ef458da7237
Clement Lin <x4383lin@gmail.com>
2017-08-21 23:26:53 -0500

    modeB() data split

--- src/elevator/Elevator.java
+++ src/elevator/Elevator.java
@@ -9,6 +9,8 @@ public class Elevator {
 	// store all the expected output of modeB
 	private ArrayList<ArrayList<Integer>> modeBList= new ArrayList<ArrayList<Integer>>();
 	
+	boolean up; // default: if requested floor - initial floor > 0
+	
 	 
 	public void modeA(String line){ 
 		
@@ -16,14 +18,15 @@ public class Elevator {
 		ArrayList<Integer> ls = new ArrayList<Integer>();  // store the output
 		String[] split = line.split("-|,|:"); // 1-5,1-6 => 1 5 1 6
 		int[] numbers = new int[split.length];
-		int mod; // modifier = -1: when nextFloor - currentFloor < 0 
 		
+		int mod; // modifier = -1: when nextFloor - currentFloor < 0 
+                 //            1 : when nextFloor - currentFloor > 0		
 		
 		for(int i = 0; i < split.length ; i++){
 			//Integer floor = Integer.valueOf(split[i]);  // String to Integer
 			numbers[i] = Integer.valueOf(split[i]);
 			ls.add(numbers[i]);
-			if(i != 0){  // 10 8 1
+			if(i != 0){  // calculate distance
 				if(numbers[i] - numbers[i-1] >= 0){
 					   mod = 1;
 				   }
@@ -35,10 +38,10 @@ public class Elevator {
 			}
 			
 		}
-		System.out.print(ls);
-		System.out.println(" distance:" + distance);
+//		System.out.print(ls);
+//		System.out.println(" distance:" + distance);
 		ls.add(0,distance); // add distance at the index0
-		System.out.println(ls);
+//		System.out.println(ls);
 		modeAList.add(ls);
 		//System.out.println("list of list :" + modeAList);
 	
@@ -46,8 +49,40 @@ public class Elevator {
 		
 	}
 	
-	public void modeB(String line){
+	public void modeB(String line){ // EX: input  7:11-6,10-5,6-8,7-4,12-7,8-9
 		
+		int distance = 0; // elevator total travel distance
+		ArrayList<Integer> ls = new ArrayList<Integer>();  // store the output
+		String startPos = line.substring(0,line.indexOf(":")); // 7, separate startPos with the rest
+		String floorRequest = line.substring(line.indexOf(":")+1); // the rest
+		String[] split = floorRequest.split("-|,|:"); // 11 6 10 5 6 8 7 4 12 7 8 9
+		int[] numbers = new int[split.length];
+		//System.out.println(startPos);
+		
+		for(int i = 0; i < split.length ; i++){ // String[] to Integer[]
+			
+			numbers[i] = Integer.valueOf(split[i]);
+			System.out.print(numbers[i]+ " ");
+		}
+		
+		
+	
+		
+	}
+	
+	public void setInitialDirection(int[] numbers){
+		if(numbers[1] - numbers[0] > 0){  
+			up = true;
+		}else{
+			up = false;
+		}
+	}
+	
+	public boolean checkDirection(int i, int d){ // initial floor and destination
+		if(d - i > 0)
+			return true;  // going up
+		else
+			return false; // going down
 	}
 	
 	public ArrayList<ArrayList<Integer>> getModeAList(){
